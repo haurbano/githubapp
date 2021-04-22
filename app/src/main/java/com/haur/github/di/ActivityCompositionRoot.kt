49a899f11784e0d584b73e0f50c2a8505be6dcd7
hurbanobenav <hamiltonandres.urbanobenavides@mercadolibre.com.co>
2021-04-21 21:37:02 -0500

    Imports deeted

--- app/src/main/java/com/haur/github/di/ActivityCompositionRoot.kt
+++ app/src/main/java/com/haur/github/di/ActivityCompositionRoot.kt
@@ -1,14 +1,9 @@
 package com.haur.github.di
 
-import android.view.LayoutInflater
 import com.haur.github.repositories.domain.FetchRepositoriesUseCase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 
 class ActivityCompositionRoot(
     private val appCompositionRoot: AppCompositionRoot,
-    private val layoutInflater: LayoutInflater
     ) {
 
     val fetchRepositoriesUseCase
